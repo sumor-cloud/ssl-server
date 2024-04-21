c288299ea6538545b137c42e3e1cee340afe0d13
dennis <dennis@sumor.com>
2024-04-21 15:41:03 +0800

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,21 @@
 // port number prefix is 103
 
 import {
+  afterEach,
+  beforeEach,
   describe, expect, it
 } from '@jest/globals'
 
 import createApp from '../index.js'
+import cleanUpSSL from './utils/cleanUpSSL.js'
 
 describe('Expose App', () => {
+  beforeEach(async () => {
+    await cleanUpSSL()
+  })
+  afterEach(async () => {
+    await cleanUpSSL()
+  })
   it('Verify expose type', async () => {
     expect(typeof createApp).toStrictEqual('function')
     const app = createApp()
